5fd934d303e0b36a5697d1fe875300fbb47f0b0d
cone26 <sight41siyah@gmail.com>
2023-11-26 22:15:08 +0900

    create a websocket server with a http server(express server)

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import {fileURLToPath} from 'url';
+import http from 'http';
+import { WebSocketServer } from 'ws';
 
 const app = express();
 const __dirname = fileURLToPath(new URL(".", import.meta.url))
@@ -9,6 +11,15 @@ app.use('/public',express.static(`${__dirname}public`))
 
 app.get('/',(req,res)=>res.render('home'))
 const port = 3000;
-const handleListen = () => console.log(`Listening on http://localhost:${port}`)
 
-app.listen(port, handleListen);
+const handleListen = () => console.log(`Listening on http://localhost:${port}`)
+// app.listen(port, handleListen);
+
+//http server
+const server = http.createServer(app);
+// create the ws server on the top of the http server
+const wss = new WebSocketServer({ server });
+// passed http server to WebSocket.Server() as a parameter
+// => make both of servers are able to run in same port.
+server.listen(port, handleListen);
+
